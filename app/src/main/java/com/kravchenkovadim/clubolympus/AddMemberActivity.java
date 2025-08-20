ad4bae3198b01180d25127d5645f27b9a8598bfa
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2025-08-20 16:43:53 +0300

    add spinner, array

--- app/src/main/java/com/kravchenkovadim/clubolympus/AddMemberActivity.java
+++ app/src/main/java/com/kravchenkovadim/clubolympus/AddMemberActivity.java
@@ -1,6 +1,9 @@
 package com.kravchenkovadim.clubolympus;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class AddMemberActivity extends AppCompatActivity {
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText groupNameEditText;
+    private Spinner genderSpinner;
+    private int gender = 0;
+    private ArrayAdapter spinnerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,13 @@ public class AddMemberActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        firstNameEditText = findViewById(R.id.firstNameEdit);
+        lastNameEditText = findViewById(R.id.lastNameEdit);
+        groupNameEditText = findViewById(R.id.groupEdit);
+        genderSpinner = findViewById(R.id.spinnerID);
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
     }
 }
