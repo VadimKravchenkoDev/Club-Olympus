727dbda532dcc17c4b8f55b3497fcf85bf2d19c4
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2025-08-20 17:49:21 +0300

    add menu

--- app/src/main/java/com/kravchenkovadim/clubolympus/AddMemberActivity.java
+++ app/src/main/java/com/kravchenkovadim/clubolympus/AddMemberActivity.java
@@ -1,17 +1,23 @@
 package com.kravchenkovadim.clubolympus;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
 
 public class AddMemberActivity extends AppCompatActivity {
     private EditText firstNameEditText;
@@ -40,5 +46,47 @@ public class AddMemberActivity extends AppCompatActivity {
         spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(spinnerAdapter);
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedGender = (String) parent.getItemAtPosition(position);
+                if(!TextUtils.isEmpty(selectedGender)){
+                    if(selectedGender.equals("Male")){
+                        gender = 1;
+                    } else if(selectedGender.equals("Female")){
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.save_member) {
+            // saveMember();
+            return true;
+        } else if (id == R.id.delete_member) {
+            return true;
+        } else if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
